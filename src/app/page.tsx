6cec2b9169dc7564f0bb657e1af18f620f8e36db
user <user@MacBook-Pro-user.local>
2023-10-02 20:27:29 +0300

    Add tailwind design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,53 @@
 export default function Home() {
   return (
     <div className="h-screen grid grid-rows-[auto_1fr_auto] text-center">
-      <header className="text-2xl m-8">My Next.js App</header>
+      <header className="text-3xl m-8">25 + 5 Clock</header>
 
       <main className="flex flex-col items-center">
-        <div className="w-80 h-96 bg-red-400"></div>
+        <div className="w-56 h-auto bg-slate-500 rounded-md p-2 m-1">
+          <h2 id="timer-label" className="text-xl p-2">
+            Session
+          </h2>
+          <div id="time-left" className="p-1">
+            25:00
+          </div>
+          <button id="start_stop" className="py-1 px-4">
+          ⏯
+          </button>
+          <button id="reset" className="py-1 px-4">
+          ↺
+          </button>
+        </div>
+
+        <div className="w-56 h-auto bg-slate-700 rounded-md p-2 m-1">
+          <h2 id="session-label" className="text-xl p-2">
+            Session Length
+          </h2>
+          <div id="session-length" className="p-1">
+            25
+          </div>
+          <button id="session-decrement" className="py-1 px-4">
+            ▼
+          </button>
+          <button id="session-increment" className="py-1 px-4">
+            ▲
+          </button>
+        </div>
+
+        <div className="w-56 h-auto bg-slate-700 rounded-md p-2 m-1">
+          <h2 id="break-label" className="text-xl p-2">
+            Break Length
+          </h2>
+          <div id="break-length" className="p-1">
+            5
+          </div>
+          <button id="break-decrement" className="py-1 px-4">
+            ▼
+          </button>
+          <button id="break-increment" className="py-1 px-4">
+            ▲
+          </button>
+        </div>
       </main>
 
       <footer className="text-sm m-4">
